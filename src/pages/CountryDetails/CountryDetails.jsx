18785916c81a6dba9ee841eee7944f95e43c712b
Valentin <valentin.monteiro@orange.fr>
2023-12-04 16:09:35 +0100

    Affichage des détails

--- src/pages/CountryDetails/CountryDetails.jsx
+++ src/pages/CountryDetails/CountryDetails.jsx
@@ -1,37 +1,84 @@
+import { useState, useEffect } from 'react';
+import {Link, useParams} from 'react-router-dom';
 import './CountryDetails.css';
 
 export default function CountryDetails() {
+    const {country} = useParams();
+    const [countryDetails, setCountryDetails] = useState();
+    const [borderCountries, setBorderCountries] = useState([]);
+
+
+    useEffect(() => {
+        getCountryDetails(country);        
+    }, [])
+
+    useEffect(() => {        
+        getCountryDetails(country);
+    }, [country])
+
+    useEffect(() => {
+        if(countryDetails) {
+            if(countryDetails.borders) {
+
+                const codes = countryDetails.borders.join(',');
+                fetch(`https://restcountries.com/v3.1/alpha?codes=${codes}`)
+                .then(res => res.json())
+                .then(data => setBorderCountries(data))
+                .catch(err => console.log(err));
+            }
+        }
+        console.log(borderCountries);
+    }, [countryDetails])
+
+    const getCountryDetails = (country) => {
+        if(country) {
+            fetch(`https://restcountries.com/v3.1/name/${country}`)
+            .then(res => res.json())
+            .then(data => setCountryDetails(data[0]))
+            .catch(err => console.log(err));
+        }
+    }
+
   return (
     <section>
         <div className="backBtn">
             <p>Back</p>
         </div>
+        {
+            countryDetails &&
         <div className="country">
-            <img src="https://cdn.pixabay.com/photo/2012/04/11/15/19/france-28463_1280.png" alt="" />
+            <img src={countryDetails.flags.png} alt={`Drapeau de ${countryDetails.name.common}`} />
             <div className="infos">
-                <h2>France</h2>
+                <h2>{countryDetails.name.common}</h2>
                 <div className="details">
                     <div>
-                        <p><strong>Native Name: </strong> French</p>
-                        <p><strong>Population: </strong> 70.000.000</p>
-                        <p><strong>Region: </strong> Europe</p>
-                        <p><strong>Sub Region: </strong> Western Europe</p>
-                        <p><strong>Capital: </strong> Paris</p>
+                        <p><strong>Native Name: </strong> {Object.values(countryDetails.name.nativeName)[0].common}</p>
+                        <p><strong>Population: </strong> {countryDetails.population}</p>
+                        <p><strong>Region: </strong> {countryDetails.region}</p>
+                        <p><strong>Sub Region: </strong> {countryDetails.subregion}</p>
+                        <p><strong>Capital: </strong> {countryDetails.capital}</p>
                     </div>
                     <div>
-                        <p><strong>Top Level Domain: </strong> .fr</p>
-                        <p><strong>Currencies: </strong> Euro</p>
-                        <p><strong>Languages: </strong> French</p>
+                        <p><strong>Top Level Domain: </strong> {countryDetails.tld}</p>
+                        {/* <p><strong>Currencies: </strong> {country.currencies}</p> */}
+                        <p><strong>Languages: </strong> {Object.values(countryDetails.languages)[0]}</p>
                     </div>
                 </div>
                 <div className="neighbours">
                     <p><strong>Border Countries: </strong></p>
-                    <button>Belgium</button>
+                    {
+                        borderCountries.length > 0 &&
+                        borderCountries.map((country, index) => (
+                            <button><Link key={index} to={`/details/${country.name.common}`}>{country.name.common}</Link></button>
+                        ))
+                    }
+                    {/* <button>Belgium</button>
                     <button>Germany</button>
-                    <button>Netherlands</button>
+                    <button>Netherlands</button> */}
                 </div>
             </div>
         </div>
+        }
     </section>
   )
 }
